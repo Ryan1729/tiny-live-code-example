98935d20482c10038ff9d0af5a65e01446691ea5
Ryan1729 <Ryan1729@gmail.com>
2017-07-11 00:53:00 -0600

    move the bindings into their own crate, to speed up compile times
    Signed-off-by: Ryan1729 <Ryan1729@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-mod gl {
-    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-}
+extern crate open_gl_bindings;
+
+use open_gl_bindings::gl;
 
 fn main() {
     println!("It least doesn't explode at least!");
